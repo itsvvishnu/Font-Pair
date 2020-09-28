91bd0da60dab384fa0152568734203b6d7eff5ba
itsvvishnu <vishnushanmukan@gmail.com>
2020-09-28 10:49:05 +0530

    minor fix

--- app/src/views/Wrapper.vue
+++ app/src/views/Wrapper.vue
@@ -8,8 +8,7 @@
           Hacktoberfest is a month-long celebration of open source software run by DigitalOcean in partnership with GitHub and Twilio. Hacktoberfest is open to everyone in our global community! Five quality pull requests must be submitted to public GitHub repositories.
         </p>
         <div class="author">
-          <span v-if="$store.state.name != undefined" :style="$store.state.secondary">Fonts picked by</span>
-          <span v-else></span>
+          <span v-show="$store.state.name != undefined" :style="$store.state.secondary">Fonts picked by</span>
           <a :href="$store.state.git" :style="$store.state.secondary" class="git">{{$store.state.name}}</a>
         </div>
       </div>
