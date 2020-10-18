baf2ad0e1600fbf6d01afa563d8eadab85cca9ab
madmaxWMFU <yasmaks@gmail.com>
2020-10-18 14:55:33 +0300

    added fonts

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -1,3 +1,4 @@
 #### Name: [Vishnu V](https://github.com/itsvvishnu)
 - Bio: FrontEnd Developer
 - Github: [Vishnu V](https://github.com/itsvvishnu)
+- Github: [Maksym Y](https://github.com/madmaxWMFU)
--- app/src/fonts.js
+++ app/src/fonts.js
@@ -7,6 +7,20 @@ export const fonts = () =>{
         secondary:'Open Sans',
         git:'itsvvishnu',
         url:'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&family=Open+Sans&display=swap'
+      },
+      {
+        title:'Quando & Zilla Slab',
+        primary:'Quandot',
+        secondary:'Zilla Slab',
+        git:'madmaxWMFU',
+        url:'https://fonts.googleapis.com/css2?family=Quando&family=Zilla+Slab:ital,wght@0,400;0,500;0,600;1,400&display=swap'        
+      },
+      {
+        title:'Kalam & Lemonada',
+        primary:'Kalam',
+        secondary:'Lemonada',
+        git:'madmaxWMFU',
+        url:'https://fonts.googleapis.com/css2?family=Kalam&family=Lemonada&display=swap'        
       }
     ]
   )
