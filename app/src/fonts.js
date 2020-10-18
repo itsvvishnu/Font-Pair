71af28c566786931c377d2774981c2169b2dd6b3
Ashwin Km <kmuraliashwin@gmail.com>
2020-10-18 18:28:33 +0530

    Updated my name in CONTRIBUTORS.md file and added the fonts in the fonts.json file

--- app/src/fonts.js
+++ app/src/fonts.js
@@ -1,13 +1,20 @@
-export const fonts = () =>{
-  return (
-    [
-      {
-        title:'Montserrat & Open sans',
-        primary:'Montserrat',
-        secondary:'Open Sans',
-        git:'itsvvishnu',
-        url:'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&family=Open+Sans&display=swap'
-      }
-    ]
-  )
-} 
+export const fonts = () => {
+  return [
+    {
+      title: "Montserrat & Open sans",
+      primary: "Montserrat",
+      secondary: "Open Sans",
+      git: "itsvvishnu",
+      url:
+        "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,500;1,500&family=Open+Sans&display=swap",
+    },
+    {
+      title: "Crimson Pro & Literata",
+      primary: "Crimson Pro",
+      secondary: "Literata",
+      git: "AshKemp",
+      url:
+        "https://fonts.googleapis.com/css2?family=Crimson+Pro&family=Literata",
+    },
+  ];
+};
